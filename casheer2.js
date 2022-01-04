4e5aecc1f95470cdbe6b846c523c4702431c9c33
Matthias Baumann <matthiasbaumann@Matthiass-MBP.fritz.box>
2022-01-04 10:48:37 +0100

    casheer in right order

--- casheer2.js
+++ casheer2.js
@@ -1,17 +1,19 @@
 //Calculate the return
 
-function Return(taken,price){
+function returnMoney(taken,price){
     let returnSum=taken-price;
     let give={}
+    let names=["50er","20er","10er","5er","2er","1er",0.5,0.2,0.1,0.05,0.02,0.01]
     let coins=[50,20,10,5,2,1,0.5,0.2,0.1,0.05,0.02,0.01]
     console.log(returnSum);
     for(i=0;i<coins.length;i++){
         if(returnSum>coins[i]){
-            give[coins[i]]=Math.floor(returnSum/coins[i])
+            give[names[i]]=Math.floor(returnSum/coins[i])
             returnSum=returnSum%coins[i]
+            // console.log(give);
         } 
     }
     console.log(give);
 }
 
-Return(100,3.11)
+returnMoney(100,3.11)
