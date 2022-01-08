55ac181b03832643346d51eabff302ce208248c3
Matthias Baumann <matthiasbaumann@Matthiass-MBP.fritz.box>
2022-01-08 18:01:52 +0100

    some small changes

--- main.js
+++ main.js
@@ -6,9 +6,19 @@ window.addEventListener("keydown", function(e){
     currentKey=e.key;
     typed.innerText=e.key;
     console.log(currentKey);
+    if(currentKey=="q"){
+        q.style="background-color:green";
+    }
 })
+function changeColor(event){
+    const id=event.target.id;
+    console.log(event.taget.id);
+}
+document.body.addEventListener("keydown", changeColor)
 
-let sentences = "Was du liebst, lass frei. Kommt es zurück, gehört es dir - für immer."
+let sentences = "Was du liebst, lass frei. Kommt es zurück, gehört es dir - für immer.";
+let nextKey=sentences[0]
+console.log(nextKey);
 // window.addEventListener("keydown", function(event) {
 //     const p = document.createElement("p");
 //     p.textContent = `KeyboardEvent: key='${event.key}' | code='${event.code}'`;
