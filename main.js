ed6b473c9d9f53b8e0a3f45744b4c8e1019e115b
Matthias Baumann <matthiasbaumann@Matthiass-MacBook-Pro.fritz.box>
2022-01-09 20:06:55 +0100

    Keybord fully working

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let currentKey;
-const allKeys=["q","w","e","r","t","z","u","i","o","p","ue","a","s","d","f","g","h","j","k","l","oe","ae","y","x","c","v","b","n","m"];
+const allKeys=["q","w","e","r","t","z","u","i","o","p","ue","a","s","d","f","g","h","j","k","l","oe","ae","y","x","c","v","b","n","m",",","."," "];
 let counter=0
 let sentences = "was du liebst, lass frei. kommt es zurück, gehört es dir - für immer!";
 let nextKey=sentences[0];
@@ -7,37 +7,32 @@ let typed=document.getElementById("typed");
 window.addEventListener("keydown", function(e){
     currentKey=e.key;
     typed.innerText=e.key;
-    console.log("currentKey",currentKey);
-    console.log("nextKey",nextKey);
     if(currentKey!=nextKey){
         for(i=0;i<allKeys.length;i++){
             if(currentKey==allKeys[i]){
-                document.getElementById(allKeys[i]).style.backgroundColor="pink";
+                document.getElementById(allKeys[i]).style.boxShadow="3px 3px 5px red inset";
             }else{
-                document.getElementById(allKeys[i]).style.backgroundColor="grey";
+                document.getElementById(allKeys[i]).style.boxShadow="3px 3px 5px black inset";
                 }
         }
     }
     if(currentKey==nextKey){
         for(i=0;i<allKeys.length;i++){
             if(currentKey==allKeys[i]){
-                document.getElementById(allKeys[i]).style.backgroundColor="lightgreen";
+                document.getElementById(allKeys[i]).style.boxShadow="3px 3px 5px lightgreen inset";
             }else{
-                document.getElementById(allKeys[i]).style.backgroundColor="grey";
+                document.getElementById(allKeys[i]).style.boxShadow="3px 3px 5px black inset";
                 }
         }
         counter ++
         nextKey=sentences[counter]
         for(i=0;i<allKeys.length;i++){
             if(nextKey==allKeys[i]){
-                document.getElementById(allKeys[i]).style.color="green";
+                document.getElementById(allKeys[i]).style.backgroundColor="green";
             }else{
-                document.getElementById(allKeys[i]).style.color="black";
+                document.getElementById(allKeys[i]).style.backgroundColor="grey";
             }
         }
-        
-        console.log("counter",counter);
-        console.log("nextKey",nextKey);
     }
 })
 
